dc0f9805b417539fad6ab7689126ed6797839f82
kinokoio <kinokoio@hushmail.com>
2015-05-17 14:58:59 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sbrowser
-Simple browser made using QT5 and QtWebKit. Similar to surf.
+sbrowser is a web browser written in Python using Qt5 libraries. It is similar to surf.
+
+Features:
+	- Lightweight: Uses ~15MB of memory.
+	- Fast
+	- Simple: Is aimed to never be +100 lines of code.
+	- Uses QtWebKit for Qt5
+	- You can change the browsed page from inside the program (opposite to surf)
+	- Has a back button. 
---
+++ sbrowser.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebKitWidgets import *
+ 
+class Form(QWidget):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+ 
+        nameLabel = QLabel("URI")
+        self.nameLine = QLineEdit()
+        self.backButton = QPushButton("<-")
+        self.webview = QWebView()
+ 
+        HLayout1 = QHBoxLayout()
+        HLayout1.addWidget(self.backButton)
+        HLayout1.addWidget(nameLabel)
+        HLayout1.addWidget(self.nameLine)
+ 
+        self.nameLine.returnPressed.connect(self.requestUri)
+        self.backButton.clicked.connect(self.goBack)
+        self.webview.loadProgress.connect(self.loading)
+        self.webview.loadFinished.connect(self.changePage)
+ 
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(HLayout1, 0, 1)
+        mainLayout.addWidget(self.webview, 1, 1)
+ 
+        self.setLayout(mainLayout)
+        self.setWindowTitle("sbrowser")
+ 
+    def requestUri(self):
+        url = QUrl(self.nameLine.text())
+        if url.isValid():
+            self.webview.load(url)
+        else:
+            self.setWindowTitle("URI not valid - sbrowser")
+
+    def loading(self):
+        self.setWindowTitle("Loading" + " - sbrowser")
+
+    def changePage(self):
+        self.setWindowTitle(self.webview.url().url() + " - sbrowser")
+
+    def goBack(self):
+        self.webview.back()
+
+if __name__ == '__main__':
+    import sys
+ 
+    app = QApplication(sys.argv)
+ 
+    screen = Form()
+    screen.show()
+ 
+    sys.exit(app.exec_())
